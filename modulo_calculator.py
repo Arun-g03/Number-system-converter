b30e6bc18ea1af86b846901607f6b259bd98fadc
Arun-g03 <127246519+Arun-g03@users.noreply.github.com>
2023-03-07 19:38:11 +0000

    Add files via upload

---
+++ modulo_calculator.py
@@ -0,0 +1,13 @@
+print("\n-----------")
+print("Modulo")
+# Prompt user for input
+while True:
+    num1 = int(input("Enter numbers for modulo: "))
+    num2 = int(input("Enter second numbers for modulo: "))
+
+    mod = num1%num2
+    
+    print(mod)
+
+    
+
